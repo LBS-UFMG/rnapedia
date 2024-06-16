2a82a9bf51bedaf4a19ff1d04821fcc6458f01c9
Diego Mariano <diogohenks@hotmail.com>
2024-06-16 11:45:00 -0300

    configuração inicial

---
+++ app/Views/home.php
@@ -0,0 +1,6 @@
+<?= $this->extend('template') ?>
+<?= $this->section('conteudo') ?>
+<!-- Conteúdo personalizado -->
+<p>Olá mundo!</p>
+<!-- / FIM Conteúdo personalizado -->
+<?= $this->endSection() ?>
